9b847e77b0e6fa91b0cd98f4af0863dc053de1f4
Daniel Bentz <db.daniel.bentz@gmail.com>
2025-01-06 08:12:33 +0900

    added swagger openAPI

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,41 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule)
+
+  const config = new DocumentBuilder()
+    // .addBearerAuth(
+    //   {
+    //     type: 'http',
+    //     scheme: 'bearer',
+    //     bearerFormat: 'JWT',
+    //   },
+    //   'backend-jwt-auth',
+    // )
+    // .addBearerAuth(
+    //   {
+    //     type: 'http',
+    //     scheme: 'bearer',
+    //     bearerFormat: 'JWT',
+    //   },
+    //   'backend-jwt-refresh',
+    // )
+    .setTitle('Spaceflix APIs')
+    .setDescription('Backend APIs consumed by the Spaceflix application(s)')
+    .setVersion('1.0')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+      docExpansion: 'none',
+      tagsSorter: 'alpha',
+    },
+  })
+
+  await app.listen(process.env.PORT ?? 3000)
 }
-bootstrap();
+bootstrap()
