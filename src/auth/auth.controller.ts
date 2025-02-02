3212ab095f2010c512bcbd06a231f32117dfbfd8
Daniel Bentz <db.daniel.bentz@gmail.com>
2025-02-02 17:43:23 +0900

    temp: testing i19n and middleware

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -94,4 +94,16 @@ export class AuthController {
   @ApiOkResponse({ description: 'Returned if the refresh token has been removed.' })
   @ApiNotFoundResponse({ description: 'Returned if the token does not exist.' })
   async testToken(@Req() req: RequestWithUser): Promise<void> {}
+
+  @Get('test-access-token2')
+  @SpaceflixController({ pathPrefix: '', tag: 'auth' })
+  @ApiOkResponse({ description: 'Returned if the refresh token has been removed.' })
+  @ApiNotFoundResponse({ description: 'Returned if the token does not exist.' })
+  async testToken2(@Req() req: RequestWithUser): Promise<void> {}
+
+  @Get('test-access-token3')
+  @SpaceflixController({ pathPrefix: '', tag: 'auth' })
+  @ApiOkResponse({ description: 'Returned if the refresh token has been removed.' })
+  @ApiNotFoundResponse({ description: 'Returned if the token does not exist.' })
+  async testToken3(@Req() req: RequestWithUser): Promise<void> {}
 }
