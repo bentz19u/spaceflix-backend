08d4bac20629e219952f552d8d4a635964e034ed
Daniel Bentz <db.daniel.bentz@gmail.com>
2025-04-21 18:26:26 +0900

    added e2e tests for POST /users

--- test/users/users.e2e-spec.ts
+++ test/users/users.e2e-spec.ts
@@ -3,6 +3,8 @@ import request from 'supertest'
 import { E2eHelper } from './common/e2e-helper'
 import { PostStep1RequestDto } from '../../src/users/dtos/post-step1-request.dto'
 import { LoginRequestDto } from '@auth/dtos/login-request.dto'
+import { UserPlanEnum } from '../../src/common/enums/plan.enum'
+import { CreateUserRequestDto } from '../../src/users/dtos/create-user-request.dto'
 
 describe('AppController (e2e)', () => {
   let app: INestApplication
@@ -104,11 +106,79 @@ describe('AppController (e2e)', () => {
     })
 
     it(`204 - Email and Password valid`, async () => {
-      const requestDto: LoginRequestDto = {
+      const requestDto: PostStep1RequestDto = {
         email: 'daniel.bentz@gmail.com',
         password: 'zr9hQBt2EcjT',
       }
       await request(app.getHttpServer()).post('/users/step1').send(requestDto).expect(HttpStatus.NO_CONTENT)
+    })
+  })
+
+  describe('POST /users', () => {
+    it(`400 - email not valid`, async () => {
+      const requestDto: CreateUserRequestDto = {
+        email: 'daniel.bentz',
+        password: 'Password1234',
+        plan: UserPlanEnum.STANDARD,
+      }
+
+      await request(app.getHttpServer())
+        .post(`/users`)
+        .send(requestDto)
+        .expect(HttpStatus.BAD_REQUEST)
+        .expect((res) => {
+          expect(res.body.error).toBeDefined()
+          expect(res.body.error.code).toBe('client-0000')
+          expect(res.body.error.description).toBeDefined()
+          expect(res.body.error.description[0].value).toBe('daniel.bentz')
+        })
+    })
+
+    it(`400 - password is not valid`, async () => {
+      const requestDto: CreateUserRequestDto = {
+        email: 'daniel.bentz@gmail.com',
+        password: '1234',
+        plan: UserPlanEnum.STANDARD,
+      }
+
+      await request(app.getHttpServer())
+        .post(`/users`)
+        .send(requestDto)
+        .expect(HttpStatus.BAD_REQUEST)
+        .expect((res) => {
+          expect(res.body.error).toBeDefined()
+          expect(res.body.error.code).toBe('client-0000')
+          expect(res.body.error.description).toBeDefined()
+          expect(res.body.error.description[0].value).toBe('1234')
+        })
+    })
+
+    it(`400 - plan is not valid`, async () => {
+      const requestDto = {
+        email: 'daniel.bentz@gmail.com',
+        password: 'zr9hQBt2EcjT',
+        plan: 'toto',
+      }
+
+      await request(app.getHttpServer())
+        .post(`/users`)
+        .send(requestDto)
+        .expect(HttpStatus.BAD_REQUEST)
+        .expect((res) => {
+          expect(res.body.error).toBeDefined()
+          expect(res.body.error.code).toBe('client-0000')
+          expect(res.body.error.description).toBeDefined()
+          expect(res.body.error.description[0].value).toBe('toto')
+        })
+    })
+
+    it(`204 - Email and Password valid`, async () => {
+      const requestDto: CreateUserRequestDto = {
+        email: 'daniel.bentz@gmail.com',
+        password: 'zr9hQBt2EcjT',
+        plan: UserPlanEnum.STANDARD,
+      }
+      await request(app.getHttpServer()).post('/users').send(requestDto).expect(HttpStatus.CREATED)
     })
   })
 })
